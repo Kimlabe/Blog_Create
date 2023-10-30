d9219078796a020963d72b10f8e5f5abc9903a47
Kimlabe <labe1827@gmail.com>
2023-10-31 01:04:44 +0900

    feat : 블로그 글 삭제 기능 추가

--- src/main/java/me/hataejong/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/hataejong/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,15 @@
 package me.hataejong.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.hataejong.springbootdeveloper.domain.Article;
 import me.hataejong.springbootdeveloper.dto.ArticleListViewResponse;
+import me.hataejong.springbootdeveloper.dto.ArticleViewResponse;
 import me.hataejong.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -25,4 +29,17 @@ public class BlogViewController {
 
         return "articleList"; // resource/templates/articleList.html 뷰 조회
     }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+                // └ 인자 id에 URI로 넘어온 값을 받아 finById()메서드로 넘겨
+                // └ 글을 조회하고, 화면엣허 사용할 모델에 데이터를 저장한 다음,
+                // └ 보여줄 화면의 템플릿 이름을 반환합니다.
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
 }
+
